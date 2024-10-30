360f1eae87cd3818fe229d877e5ce7894fa27615
Dimitris <jvortelinas@gmail.com>
2024-10-30 09:45:37 +0100

    new

---
+++ src/app/model/Employee.ts
@@ -0,0 +1,26 @@
+
+
+
+export class EmployeeModel {
+    empId: number;
+    name: string;
+    city: string;
+    state: string;
+    emailId: string;
+    contactNum: string;
+    address: string
+
+
+    constructor() {
+        
+        this.empId = 1,
+            this.name = '',
+            this.city = '',
+            this.state = '',
+            this.emailId = '',
+            this.contactNum = '',
+            this.address = ''
+
+    }
+
+}
